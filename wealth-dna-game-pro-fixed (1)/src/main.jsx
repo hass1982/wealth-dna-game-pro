4ec9f1214d54ae9b763a8802c9315c6ee9925200
hass1982 <benaissa.hassine@gmail.com>
2025-05-18 15:56:46 +0400

    Add files via upload

---
+++ wealth-dna-game-pro-fixed (1)/src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import WealthDNAPro from './WealthDNAPro';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WealthDNAPro />
+  </React.StrictMode>
+);
